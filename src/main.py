5846c58fa04203091cadc6658fa60cae0f4159b1
Christian Lugo Ramírez <chrislugram@gmail.com>
2025-03-17 22:56:22 +0100

    feat: WIP

--- src/main.py
+++ src/main.py
@@ -1,1 +1,5 @@
-print("Hello world")
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# TODO include env variables
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
