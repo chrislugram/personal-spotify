a8439831040e3ddb5f7845c779fae8f45ee7bebb
Christian Lugo <chrislugram@gmail.com>
2025-05-05 16:06:49 +0200

    feat: Including setting class

---
+++ src/config/settings.py
@@ -0,0 +1,40 @@
+"""
+This file contains the class for managing the settings
+"""
+
+from pydantic import BaseModel
+from pydantic_settings import (BaseSettings, SettingsConfigDict,
+                               YamlConfigSettingsSource)
+
+
+class EnvironmentSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    SPOTIFY_CLIENT_ID: str
+    SPOTIFY_CLIENT_SECRET: str
+    SPOTIFY_URI: str
+
+
+class StorageSettings(BaseModel):
+    base_path: str
+
+
+class YamlSettings(BaseSettings):
+    model_config = SettingsConfigDict(yaml_file="config.yaml")
+    storage: StorageSettings
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls,
+        init_settings,
+        env_settings,
+        dotenv_settings,
+        file_secret_settings,
+    ):
+        return (YamlConfigSettingsSource(settings_cls),)
+
+
+class Settings:
+    def __init__(self):
+        self.environment_settings = EnvironmentSettings()
+        self.yaml_settings = YamlSettings()
