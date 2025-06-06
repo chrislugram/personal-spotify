655a41e5b4a2358306ea34b742e4672044785417
Christian Lugo <chrislugram@gmail.com>
2025-06-06 12:55:01 +0200

    feat: Add download playlist to process

--- src/processes/get_raw_data_from_spotify.py
+++ src/processes/get_raw_data_from_spotify.py
@@ -2,12 +2,12 @@
 This file contains the process to get the raw data from spotify
 """
 
+import json
 from dataclasses import dataclass, field
-from pathlib import Path
 
 from src.processes.process import Process
-from src.services.spotify_service import SpotifyService
-from src.storage.storage import Storage
+from src.services.spotify.spotify_service import SpotifyService
+from src.services.storage.storage import Storage
 
 
 @dataclass
@@ -20,6 +20,8 @@ class GetRawDataFromSpotify(Process):
     storage: Storage = field(init=False)
 
     def __post_init__(self):
+        super().__post_init__()
+
         self.spotify_service = SpotifyService(
             client_id=self.settings.environment_settings.SPOTIFY_CLIENT_ID,
             client_secret=self.settings.environment_settings.SPOTIFY_CLIENT_SECRET,
@@ -28,8 +30,24 @@ class GetRawDataFromSpotify(Process):
         self.storage = Storage(base_path=self.settings.yaml_settings.storage.base_path)
 
     def run(self):
+        """
+        Run the process
+        """
+        # Define raw data path
+        raw_data_path = self.settings.yaml_settings.storage.raw_zone
+
         # Get all playlists from spotify
+        playlists = self.spotify_service.get_playlists()
+        playlists_bytes = json.dumps(playlists).encode("utf-8")
+        self.storage.save(
+            relative_path=raw_data_path + "/playlists.json",
+            data=playlists_bytes,
+        )
+        self.logger.info(f"User's Playlists downloaded, total {len(playlists)}")
+
         # For each playlist,
         #     Get all tracks and save to storage
         #     Save playlist to storage
+
+    def clean(self):
         pass
