121bd42b6bb1d065b9f4ba925466181f1296e7bf
Christian Lugo <chrislugram@gmail.com>
2025-06-17 13:19:58 +0200

    refactor: Update name of Process

--- src/pipelines/ingest_music_data.py
+++ src/pipelines/ingest_music_data.py
@@ -1,12 +1,12 @@
 from src.pipelines.pipeline import Pipeline
-from src.processes.extract_data_spotify import GetExtractDataSpotify
+from src.processes.extract_data_spotify import ExtractDataSpotify
 
 
 class IngestMusicData(Pipeline):
     def initialize(self):
         # Create list of steps process
         self.processes = [
-            GetExtractDataSpotify(
+            ExtractDataSpotify(
                 name="get_raw_data_from_spotify", settings=self.settings
             ),
         ]
